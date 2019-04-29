6fac7d61ef7c0f362aaab6972b3e7e8bac59322e
benedictbobis <bobis.benedict@gmail.com>
2019-04-29 13:48:03 -0600

    Added projection and query method for Employees entity

---
+++ server/src/main/java/com/benbobis/employeemanagerreactpoc/models/EmployeeProjection.java
@@ -0,0 +1,25 @@
+package com.benbobis.employeemanagerreactpoc.models;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Projection(name = "simple", types={ Employee.class })
+public interface EmployeeProjection {
+    String getFirstName();
+
+    String getLastName();
+
+    LocalDate getBirthDate();
+
+    LocalDate getHireDate();
+
+    @Value("#{target.title.name}")
+    String getTitle();
+
+    List<Salary> getSalaries();
+
+    List<Department> getDepartments();
+}
--- server/src/main/java/com/benbobis/employeemanagerreactpoc/repositories/EmployeeRepository.java
+++ server/src/main/java/com/benbobis/employeemanagerreactpoc/repositories/EmployeeRepository.java
@@ -1,11 +1,15 @@
 package com.benbobis.employeemanagerreactpoc.repositories;
 
 import com.benbobis.employeemanagerreactpoc.models.Employee;
+import com.benbobis.employeemanagerreactpoc.models.EmployeeProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(excerptProjection = EmployeeProjection.class)
 @PreAuthorize("hasAuthority('Staff')")
 public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Integer> {
+    Page<Employee> findByFirstNameStartingWith(String firstName, Pageable p);
 }
